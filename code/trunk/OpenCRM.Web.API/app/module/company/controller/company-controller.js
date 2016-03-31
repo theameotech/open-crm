ca7032a687c9805692d53544feeefe65719b1634
kamal <kamal.ameotech@gmail.com>
2016-03-31 15:23:30 +0530

    implement UI for Dashboard.

--- code/trunk/OpenCRM.Web.API/app/module/company/controller/company-controller.js
+++ code/trunk/OpenCRM.Web.API/app/module/company/controller/company-controller.js
@@ -29,23 +29,48 @@ function ($scope, growl, companyService, userService, $location) {
         $scope.ConfirmepasswordStatus = false;
     };
 
+    $scope.Test = function () {
+
+        $scope.fval = Math.floor(10 * Math.random())
+        $scope.sval = Math.floor($scope.fval * Math.random())
+
+        localStorage.setItem("f", $scope.fval);
+        localStorage.setItem("s", $scope.sval);
+    }
 
 
     $scope.CreateCompany = function () {
+        var f = localStorage.getItem("f");
+        var s = localStorage.getItem("s");
+        var entervalue = $scope.valu;
+
         if ($.fn.validateForceFully($("#registerform")) == true) {
-            companyService.addCompany($scope.CompanyModel)
-                    .then(function (response) {
-                        console.log(response);
-                        if (response.data.Success) {
-                            growl.success(response.data.Message);
-                        }
-                        else {
-                            growl.error(response.data.Message);
-                        }
-                    }, function (err) {
-                        $scope.Error = "We are unable to create user at this time, Please try again later.";
-                    });
+         
+
+            if ((parseInt(entervalue) + parseInt(s)) === parseInt(f)) {
+
+                companyService.addCompany($scope.CompanyModel)
+                        .then(function (response) {
+                            console.log(response);
+                            if (response.data.Success) {
+                                growl.success(response.data.Message);
+                            }
+                            else {
+                                growl.error(response.data.Message);
+                            }
+                        }, function (err) {
+                            $scope.Error = "We are unable to create user at this time, Please try again later.";
+                        });
+
+            }
+            else {
+                console.log("user invalid hai");
+            }
+
+            //if ($.fn.validateForceFully($("#registerform")) == true) {
+
         }
+
     };
     $scope.Countries = [];
     $scope.GetCountries = function () {
@@ -56,6 +81,8 @@ function ($scope, growl, companyService, userService, $location) {
     };
 
     $scope.GetCountries();
+
+    $scope.Test();
 }]);
 
 
---
+++ code/trunk/OpenCRM.Web.API/app/module/register/service/login-service.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module('acApp')
+    .service('loginService', ['$http', 'baseUrl', function ($http, baseUrl) {
+
+        var loginService = {};
+
+        loginService.login = function (userModel) {
+            return $http.post(baseUrl + '/api/user/login', userModel);
+        };
+
+        loginService.logout = function (userModel) {
+            return $http.post(baseUrl + '/api/user/logout', userModel);
+        };
+
+        return loginService;
+
+    }])
